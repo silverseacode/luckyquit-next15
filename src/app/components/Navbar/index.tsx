354390a4ac9c4bf6b6a4bd03e5eab9190efec96b
silverseacode <businessfranconicolascoronel94@gmail.com>
2024-11-26 19:56:49 -0300

    dark logo

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
   return (
     <nav className="flex flex-row items-center mt-5">
       <div className="cursor-pointer flex-row flex items-center">
-        <Image width={60} height={60} alt="Lucky Quit Logo" src={"/logo.png"} />
+        <Image width={60} height={60} alt="Lucky Quit Logo" src={isDarkMode ? "/logo-dark.png":"/logo.png"} />
         <span className={"w-[15rem]  font-bold text-[1.5rem] dark:text-white"}>Lucky Quit</span>
       </div>
       <NavbarItems/>
